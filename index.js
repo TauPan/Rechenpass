29910e8fda71d6096847a27a3604d60cf6a0c286
Friedrich Delgado <TauPan@users.noreply.github.com>
2017-11-10 19:38:14 +0100

    (und das element an den body zu hängen)

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const inverses = {
 for (var i = 0; i++; i<num) {1
   const p = document.createElement('p');
   p.innerHTML = problem();
+  document.querySelector('body').appendChild(p);
 };
 
 function problem() {
