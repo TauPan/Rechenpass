4ba153f8ccb0253929164db3583b8f4aee6f39ec
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2017-11-10 22:26:58 +0100

    fix a few (syntax) errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const num = 10;
 const limit = 20;
-const operators = ['+', '-']
+const operators = ['+', '-'];
 const problems = {
   '+': addition,
   '-': subtraction
@@ -15,18 +15,18 @@ for (var i = 0; i<num; i++) {1
 function problem() {
   const operation = operators[Math.floor(Math.random() * operators.length)];
   const solution = Math.floor(Math.random() * limit + 1);
-  const operator1, operator2 = problems[operator](solution);
+  const [operator1, operator2] = problems[operation](solution);
   return `${operator1} ${operation} ${operator2} = ${solution}`;
 }
 
 function addition(solution) {
   const operator1 = Math.floor(Math.random() * solution + 1);
   const operator2 = solution - operator1;
-  return operator1, operator2;
+  return [operator1, operator2];
 }
 
 function subtraction(solution) {
   const operator1 = Math.floor(Math.random() * limit + 1);
   const operator2 = operator1 - solution;
-  return operator1, operator2;
+  return [operator1, operator2];
 }
